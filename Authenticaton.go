86f680bee6989c76607da14c30dc6e5276c34937
Unknown <ultima51@yahoo.com>
2018-02-15 11:48:25 +0700

    fix main on authentication

--- Authenticaton.go
+++ Authenticaton.go
@@ -13,7 +13,7 @@ const (
 	DB_NAME     = "excite_mobile_dev"
 )
 
-func main() {
+func MainAuthentication() {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
 		DB_USER, DB_PASSWORD, DB_NAME)
 	db, err := sql.Open("postgres", dbinfo)
